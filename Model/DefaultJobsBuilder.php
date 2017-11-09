633209d3c4bf780860f4a3a172a455dcdb1cf499
Ainars <ainarsd@gmail.com>
2017-11-09 20:59:58 +0100

    Feature: defaultJobBuilder has an SkipArray of Regexp strings

--- Model/DefaultJobsBuilder.php
+++ Model/DefaultJobsBuilder.php
@@ -7,17 +7,24 @@ use Ainars\WebCrawler\Model\Job;
 use GuzzleHttp\Url;
 use simplehtmldom_1_5\simple_html_dom;
 
-class DefaultJobsBuilder implements JobsBuilderInterface
-{
+class DefaultJobsBuilder implements JobsBuilderInterface {
 
 	/**
+	 * current url
 	 * @var Url
 	 */
 	protected $baseUrl;
 
 	/**
+	 * array of regexp to check absolute URL.
+	 * If URL pass one of rules, then we skip this URL
+	 * @var string[]
+	 */
+	protected $skipRules = [];
+
+	/**
 	 * sends back all urls of links in page
-     *
+	 *
 	 * @param simple_html_dom $html
 	 * @return array
 	 */
@@ -41,24 +48,42 @@ class DefaultJobsBuilder implements JobsBuilderInterface
 		return $newJobs;
 	}
 
-	protected function _getLinkObjects($html) {
+	protected function _getLinkObjects(simple_html_dom $html)
+	{
 		return $html->find('a');
 	}
 
-	protected function _canWeSkip($url, $text = '') {
-		return $this->_isHashUrl($url) || $this->_isMail($url);
+	protected function _canWeSkip($url, $text = '')
+	{
+		if ($this->_isHashUrl($url) || $this->_isMail($url)) {
+			return true;
+		}
+
+		if (!empty($this->skipRules)) {
+			$absoluteUrl = $this->_buildAbsoluteUrl($url);
+			foreach ($this->skipRules as $rule) {
+				if (preg_match($rule, $absoluteUrl)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 
-	protected function _isHashUrl($url) {
+	protected function _isHashUrl($url)
+	{
 		return strpos($url, '#') === 0;
 	}
 
-	protected function _isMail($url) {
+	protected function _isMail($url)
+	{
 		return strpos($url, 'mailto:') === 0;
 	}
 
-	protected function _buildAbsoluteUrl($url) {
-		return (string)$this->baseUrl->combine($url);
+	protected function _buildAbsoluteUrl($url)
+	{
+		return (string) $this->baseUrl->combine($url);
 	}
 
 }
